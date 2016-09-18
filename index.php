b6b9b21fb307fe0066d133f4c47a4b0094a7f349
Marijo Novosel <marijo.novosel@foi.hr>
2016-09-18 10:26:50 +0200

    Initial commit - 01

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>WebRTC WebCam</title>
+    <link rel="stylesheet" href="css/webcam.css"/>
+    <script src="js/webcam.js"></script>
+  </head>
+  <video id="player" autoplay="true"></video>
+</html>
